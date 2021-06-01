bbc4f0ae99514aa2d86160b371d8374615790e3e
sumit <sumitmishrab@gmail.com>
2021-06-01 17:37:11 +0530

    after 3rd commit using serial wise commit(commit4)

--- tweets/views.py
+++ tweets/views.py
@@ -1,59 +1,61 @@
-
+import random
+from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
-from .models import Tweet
-import random
-from .forms import TweetForm
 from django.utils.http import is_safe_url
-from django.conf import settings
-
+from .forms import TweetForm
+from .models import Tweet
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
-
-
+# Create your views here.
 def home_view(request, *args, **kwargs):
-    
-    return render(request,"pages/home.html",context={},status = 200)
+    return render(request, "pages/home.html", context={}, status=200)
 
-def tweet_list_view(request,*args,**kwargs):
+def tweet_create_view(request, *args, **kwargs):
+    
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201) # 201 == created items
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
+        form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+    return render(request, 'components/form.html', context={"form": form})
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content,
-                    "likes": random.randint(0, 99)} for x in qs]
-                    
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
     }
     return JsonResponse(data)
-
-
-def tweet_create_view(request, *args, **kwargs):
-    form = TweetForm(request.POST or None)
-    next_url = request.POST.get("next") or None
-   
-    if form.is_valid():
-        obj = form.save(commit=False)        
-        obj.save()
-        if request.is_ajax():
-            return JsonResponse({}, status=201) # 201 == created items
-        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
-            return redirect(next_url)
-
-        form = TweetForm()
-    return render(request, 'components/form.html', context={"form": form})
-
-
-def tweet_detail_view(request, tweet_id, *args, **kwargs):   
-
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
     data = {
-        "id": tweet_id,       
+        "id": tweet_id,
     }
     status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
-        data['content']=obj.content
-        
+        data['content'] = obj.content
     except:
-        
-        data['message']="not Found"
+        data['message'] = "Not found"
         status = 404
-    return JsonResponse(data,status=status)
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
